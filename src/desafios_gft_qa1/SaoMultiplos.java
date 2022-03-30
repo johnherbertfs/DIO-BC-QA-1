f4ed55c1e59d9857647c93ca873230e57ee97b4a
johnherbertfs <johnherbertfs@gmail.com>
2022-03-30 17:28:57 -0300

    Esse deu certo no editor da DIO de prmeira.

---
+++ src/desafios_gft_qa1/SaoMultiplos.java
@@ -0,0 +1,26 @@
+package desafios_gft_qa1;
+
+import java.util.Scanner;
+
+public class SaoMultiplos {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("digite o primeiro numero:  ");
+		int n1 = scan.nextInt();
+		System.out.println("digite o segundo numero:  ");
+		int n2 = scan.nextInt();
+		
+		int temp = n2%n1; 
+		
+		if((n2%n1) == 0 || (n1%n2) == 0) {
+            System.out.println("Sao Multiplos");
+        } else {
+            System.out.println("Nao sao Multiplos");
+        }
+
+	}
+
+}
