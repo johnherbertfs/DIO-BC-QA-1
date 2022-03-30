eaf536152d4c13b9d34db3841ef43195eb6477df
johnherbertfs <johnherbertfs@gmail.com>
2022-03-30 15:32:48 -0300

    Código fuciona conforme entradas, com saídas ok.  Mas no editor da DIO dá erro "java.util.NoSuchElementException".

--- src/desafios_gft_qa1/EncaixaOuNao1.java
+++ src/desafios_gft_qa1/EncaixaOuNao1.java
@@ -6,73 +6,46 @@ public class EncaixaOuNao1 {
 
 	public static void main(String[] args) {
 		
-Scanner scan = new Scanner(System.in);
-		
-		System.out.println("digite o número de interações:  ");
+		Scanner scan = new Scanner(System.in);
+		//System.out.println("digite o número de interações:  ");
 		int vezesInteracoes = scan.nextInt(); scan.nextLine();
 		
 		String num1;
 		String num2;
-		String teste = null;
 		
 		for(int i = 0; i < vezesInteracoes; i++) {
-			System.out.println("digite o primeiro número:  ");
+			//System.out.println("digite o primeiro número:  ");
 			num1 = scan.nextLine();
-			System.out.println("digite o segundo número:  ");
+			//System.out.println("digite o segundo número:  ");
 			num2 = scan.nextLine();
 			
 			int tamanhoNum1 = num1.length() - 1;
 			int tamanhoNum2 = num2.length() - 1;
-			//tamanhoNum2 -= 1;
-			
-			while(true) {
-				if(tamanhoNum1 < 0) {
-					teste = "encaixa";
-					break;
-				}
-				if(num1.charAt(tamanhoNum1) == num2.charAt(tamanhoNum2)) {
-					//teste = "encaixa";
-					tamanhoNum1--;
-					tamanhoNum2--;
-					if(tamanhoNum2 < 0) {
-						//teste = "encaixa";
+			//tamanhoNum2 -= 1;			
+			if(tamanhoNum1 < tamanhoNum2) {
+				System.out.println("nao encaixa");
+				continue;
+			} else {
+				
+				while(true) {
+					if(num1.charAt(tamanhoNum1) == num2.charAt(tamanhoNum2)) {
+						tamanhoNum1--;
+						tamanhoNum2--;
+						if(tamanhoNum2 < 0) {
+							System.out.println("encaixa");
+							break;
+						} else {
+							continue;
+						}
+					}else {
+						System.out.println("nao encaixa");
 						break;
-					} else {
-						continue;
 					}
-				}else {
-					teste = "nao encaixa";
-					break;
 				}
-			}
-			
-			/*for(; ; --j) {
-				if(num1.charAt(j) == num2.charAt(j)) {
-					teste = "encaixa";
-					continue;
-				}
-				else {
-					teste = "nao encaixa";
-					break;
-				}
-			}*/
-			
-			if(teste.equalsIgnoreCase("encaixa")) {
-				System.out.println(teste);
-			}
-			else {
-				System.out.println(teste);
 			}
 		}
-		System.out.println("FIM!!!");
-		
-		/*
-		 * 
-		--- 0 1 2 3 4
-		
-		5 6 7 8 6 9 0
-		    7 8 6 9 0
-		*/
+		//System.out.println("FIM!!!");
+		scan.close();
 
 	}
 
