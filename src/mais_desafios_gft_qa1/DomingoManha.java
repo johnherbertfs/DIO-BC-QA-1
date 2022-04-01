8f79ddf12be157e4c7f86779f697ee71aed88e03
johnherbertfs <johnherbertfs@gmail.com>
2022-04-01 02:29:27 -0300

    Ajuste no código 'Domingo de Manhã', mas ainda não está pronto.

--- src/mais_desafios_gft_qa1/DomingoManha.java
+++ src/mais_desafios_gft_qa1/DomingoManha.java
@@ -8,21 +8,25 @@ public class DomingoManha {
 		
 		Scanner scan = new Scanner(System.in);
 		
-		System.out.println("digite uma hora:  ");
-		String horario[] = scan.nextLine().split(":");
+		//System.out.println("digite uma hora:  ");
+		//String horario[] = scan.nextLine().split(":");
 		
-		System.out.println("hora digitada:  hora " + horario[0] + ", minuto " + horario[1]);
+		//System.out.println("hora digitada:  hora " + horario[0] + ", minuto " + horario[1]);
 		
-		
-		switch(horario[0]) {
+		while(true) {
+			String horario[] = scan.nextLine().split(":");
+			switch(horario[0]) {
 			case("7"): System.out.println("Atraso maximo: " + horario[1]); break;
 			case("8"): System.out.println("Atraso maximo: " + horario[1]); break;
 			case("9"): System.out.println("Atraso maximo: " + horario[1]); break;
 			default:  System.out.println("Atraso maximo: 0");
+			}
+			scan.close();
 		}
 		
+
 		
-		scan.close();
+
 
 	}
 
