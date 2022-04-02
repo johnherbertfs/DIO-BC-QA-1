c1726801b7ee506a8567809d6bd00237837500e8
johnherbertfs <johnherbertfs@gmail.com>
2022-04-02 02:31:09 -0300

    Tetanto corrigir desafio 'Encaixa / Não Encaixa' só com IF's.

--- src/desafios_gft_qa1/EncaixaOuNao1.java
+++ src/desafios_gft_qa1/EncaixaOuNao1.java
@@ -7,31 +7,30 @@ public class EncaixaOuNao1 {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-		//System.out.println("digite o número de interações:  ");
-		int vezesInteracoes = scan.nextInt(); scan.nextLine();
+		System.out.println("digite o número de interações:  ");
+		int iteracoes = scan.nextInt(); scan.nextLine();
 		
-		String num1;
-		String num2;
+		String n1;
+		String n2;
 		
-		for(int i = 0; i < vezesInteracoes; i++) {
-			//System.out.println("digite o primeiro número:  ");
-			num1 = scan.nextLine();
-			//System.out.println("digite o segundo número:  ");
-			num2 = scan.nextLine();
+		for(int i = 0; i < iteracoes; i++) {
+			System.out.println("digite o primeiro número:  ");
+			n1 = scan.nextLine();
+			System.out.println("digite o segundo número:  ");
+			n2 = scan.nextLine();
 			
-			int tamanhoNum1 = num1.length() - 1;
-			int tamanhoNum2 = num2.length() - 1;
+			int tamanhoN1 = n1.length() - 1;
+			int tamanhoN2 = n2.length() - 1;
 			//tamanhoNum2 -= 1;			
-			if(tamanhoNum1 < tamanhoNum2) {
+			if(tamanhoN1 < tamanhoN2) {
 				System.out.println("nao encaixa");
 				continue;
 			} else {
-				
-				while(true) {
-					if(num1.charAt(tamanhoNum1) == num2.charAt(tamanhoNum2)) {
-						tamanhoNum1--;
-						tamanhoNum2--;
-						if(tamanhoNum2 < 0) {
+			/*	while(true) { 
+					if(n1.charAt(tamanhoN1) == n2.charAt(tamanhoN2)) {
+						tamanhoN1--;
+						tamanhoN2--;
+						if(tamanhoN2 < 0) {
 							System.out.println("encaixa");
 							break;
 						} else {
@@ -41,12 +40,17 @@ public class EncaixaOuNao1 {
 						System.out.println("nao encaixa");
 						break;
 					}
-				}
+				} */
+					
+				int inicio = tamanhoN1 - tamanhoN2;
+				int fim = tamanhoN1 - 1;
+				
+				System.out.println("string completa:  " + n1);
+				System.out.println("sub string:  " + n1.substring(inicio, fim));
+					
 			}
 		}
-		//System.out.println("FIM!!!");
 		scan.close();
-
 	}
 
 }
